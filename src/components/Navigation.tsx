db7fbad781584cba42ec51aa89788065e497c1b9
PPedroR <pedrorodrigues733746@gmail.com>
2025-03-29 22:23:36 +0000

    margin

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,8 +10,8 @@ export function Navigation({ scrolled }: NavigationProps) {
   const [isOpen, setIsOpen] = React.useState(false);
 
   return (
-    <nav className={`fixed z-50 transition-all duration-700 sm:p-5 mt-3 ${
-      scrolled ? 'bg-slate-900/90 backdrop-blur-sm shadow-lg mt-4 sm:w-[80%] w-[92%] sm:py-1 py-0 px-1 sm:px-2 rounded-lg mx-2' : 'bg-[#050725]' 
+    <nav className={`fixed z-50 transition-all duration-500 sm:p-5 mt-3 ${
+      scrolled ? 'bg-slate-900/90 backdrop-blur-sm shadow-lg mt-4 sm:mt-8 sm:w-[80%] w-[92%] sm:py-1 py-0 px-1 sm:px-2 rounded-lg mx-2' : 'bg-[#050725]' 
     }`}>
       <div className="container mx-auto px-4 flex justify-between items-center h-20">
         <a href="#hero" className="flex items-center">
